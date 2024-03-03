58b7c39dd3bea31fe7c603c1b107df719e2bd045
flet1234 <flet1908@gmail.com>
2024-03-03 12:26:27 +0200

    Fixed fizbuz with 0, did exercise gold 1

--- Week2/Day1/Execrcise_classFizzBuzz.py
+++ Week2/Day1/Execrcise_classFizzBuzz.py
@@ -1,9 +1,12 @@
 number=int(input("Please input a number: "))
-if number % 3==0 and number % 5==0:
-    print('FizzBuzz')
-elif number % 5==0:
-    print("Buzz")
-elif number %3==0:
-    print('Fizz')
-else :
+if number!=0:
+    if number % 3==0 and number % 5==0:
+        print('FizzBuzz')
+    elif number % 5==0:
+        print("Buzz")
+    elif number %3==0:
+        print('Fizz')
+    else :
+        print('No Fizz Buzz for you!')
+else:
     print('No Fizz Buzz for you!')      
