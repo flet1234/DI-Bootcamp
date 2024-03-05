c32948a7c1fe32474ff544735fa89f45858060af
flet1234 <flet1908@gmail.com>
2024-03-05 19:27:21 +0200

    w2d3 Timed_challenge1 and2

---
+++ Week2/Day3/Timed_challenge1.py
@@ -0,0 +1,5 @@
+# user_input='You have entered a wrong domain'
+# user_input_list=user_input.split(' ')
+# new_user_list=user_input_list[::-1]
+# new_user_input=' '.join(new_user_list)
+# print(new_user_input)
---
+++ Week2/Day3/Timed_challenge2.py
@@ -0,0 +1,11 @@
+# input=496
+# num_list=[]
+# for x in range(input):
+#     if x==0:
+#         continue
+#     elif input%x==0:
+#         num_list.append(x)
+# if sum(num_list)==input:
+#     print(True)
+# else:
+#     print(False)
