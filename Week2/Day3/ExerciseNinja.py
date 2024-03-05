dcb4b4cd94b5ad49e7c698c02e24b63b82ae2649
flet1234 <flet1908@gmail.com>
2024-03-05 19:49:27 +0200

    w2d3 ExerciseNinja

---
+++ Week2/Day3/ExerciseNinja.py
@@ -0,0 +1,28 @@
+# 1,2,3,4,5
+# manufacturers='Volkswagen, Toyota, Ford Motor, Honda, Chevrolet'
+# manufacturers_list=manufacturers.split(',')
+# print(manufacturers_list)
+# print(f'There is {len(manufacturers_list)} manufacturers in a list')
+# print(manufacturers_list[::-1])
+# total_os=0
+# total_no_is=0
+# for x in manufacturers_list:
+#     if 'o' in x.lower():
+#         total_os=total_os+1
+#     if 'i' not in x.lower():
+#         total_no_is=total_no_is+1
+# print(f'There is {total_os} manufacturers with a letter "o"')
+# print(f'There is {total_no_is} maufacturers without letter "i"')
+
+# 6,7
+
+# manufacturers=['Honda', 'Volkswagen', 'Toyota', 'Ford Motor', 'Honda', 'Chevrolet', 'Toyota']
+# original_set=set(manufacturers)
+# print(f'{','.join(original_set)}. There is {len(original_set)} manufacturers in a list now')
+
+# original_list=list(original_set)
+# new_list=[]
+# for x in original_list:
+#     new_list.append(x[::-1])
+# print (new_list)
+
