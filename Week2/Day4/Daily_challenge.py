b13fcf785dbcccbed6af16ce8f4e3af06682e0bd
flet1234 <flet1908@gmail.com>
2024-03-06 19:13:26 +0200

    w2d4_Daily_chalenge

---
+++ Week2/Day4/Daily_challenge.py
@@ -0,0 +1,48 @@
+# import string
+
+# matrix=[]
+# matrix3=[]
+# matrix_string='7iiTsxh%?i #sM $a #t%^r!'
+# matrix_list=list(matrix_string)
+# count=0
+# while count!=len(matrix_list):
+#     for x in matrix_list:
+#         count=count+1
+#         matrix3.append(x)
+#         if count%3==0:
+#             newlist=matrix3.copy()
+#             matrix.append(newlist)
+#             matrix3.pop()
+#             matrix3.pop()
+#             matrix3.pop()
+# print(matrix)
+
+# count=0
+# newmatrix=[]
+# newsemitrix=[]
+# while count!=3:
+#     for item in matrix:
+#         newmatrix.append(item[count])
+#     count=count+1
+#     if count==3:
+#         break
+# print(newmatrix)
+
+# final_list=[]
+# for x in range(len(newmatrix)):
+#     if newmatrix[x].isalpha():
+#         final_list.append(newmatrix[x])
+#         if not newmatrix[x+1].isalpha():
+#             final_list.append(' ')
+# if final_list[-1]==' ':
+#     final_list.pop(-1)
+            
+# print(final_list)
+
+# final_answer=''.join(final_list)
+# print(final_answer)
+
+
+
+
+
