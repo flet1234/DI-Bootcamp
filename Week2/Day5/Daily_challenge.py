c7768de59d2fa643c0333b032ff94538e9d0432d
flet1234 <flet1908@gmail.com>
2024-03-07 15:40:02 +0200

    w2d5 Daily_challenge

---
+++ Week2/Day5/Daily_challenge.py
@@ -0,0 +1,27 @@
+# 1
+
+# def alphabeter(string):
+#     string=string.split(',')
+#     newstring=sorted(string)
+#     sortedstring=','.join(newstring)
+#     print(sortedstring)
+#     return sortedstring
+
+# string='without,hello,bag,world'
+
+# alphabeter('without,hello,bag,world')
+
+# 2
+
+# string="Margaret's toy is a pretty dolllllllllllllllllll."
+# def bigestword(string):
+#     string_list=string.split(' ')
+#     longestword=['']
+#     for x in string_list:
+#         if len(x)>len(longestword[0]):
+#             longestword.append(x)
+#             longestword.pop(0)
+#     bigestword=longestword[0]
+#     print(bigestword)
+#     return bigestword
+# bigestword("Margaret's toy is a pretty dolllllllllllllllllll.")
