05388b236d29dd8b31a3a067aec28bae8f515f1a
flet1234 <flet1908@gmail.com>
2024-03-12 17:47:42 +0200

    d3d3 Daily_challenge_Gold

---
+++ Week3/Day3/Daily_challenge_Gold.py
@@ -0,0 +1,9 @@
+# mylist=[]
+# for x in range(5):
+#     x1=input('Name?: ')
+#     x2=int(input('Age? :'))
+#     x3=int(input('Score? :'))
+#     mylist.append((x1,x2,x3))
+
+# result=sorted(mylist,key=lambda x:(x[0],x[1],x[2]))
+# print(result)
