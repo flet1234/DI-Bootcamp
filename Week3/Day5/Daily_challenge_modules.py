29fe962e810b363a8b6ce67b45cdd129b62d844c
flet1234 <flet1908@gmail.com>
2024-03-14 13:17:48 +0200

    w3d5 Daily_modules

---
+++ Week3/Day5/Daily_challenge_modules.py
@@ -0,0 +1,26 @@
+# import requests
+# import time
+
+# x=time.time()
+# requests.get('https://google.com')
+# y=time.time()
+# result=y-x
+# print(f'To google {result}')
+
+# x=time.time()
+# requests.get('https://imdb.com')
+# y=time.time()
+# result=y-x
+# print(f'To imdb {result}')
+
+# x=time.time()
+# requests.get('https://ynet.com')
+# y=time.time()
+# result=y-x
+# print(f'To ynet {result}')
+
+# x=time.time()
+# requests.get('https://developers.institute/en/')
+# y=time.time()
+# result=y-x
+# print(f'To DI {result}')
