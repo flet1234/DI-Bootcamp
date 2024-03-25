744f9073e334b0992dd7d8759d35774c274fc8c8
flet1234 <flet1908@gmail.com>
2024-03-25 15:08:36 +0200

    w5d2 Daily_challenge Useless for loops

---
+++ Week5/Day1/Daily_challenge_Stars/script.js
@@ -0,0 +1,26 @@
+
+// for (i=0;i<6;i++){
+//     console.log('*'.repeat(i+1))}
+
+// let y=''
+//     for (x=0;x<6;x++){
+//         console.log(y+='*')
+//     }
+
+// let n='*'
+// for (i=0;i<6;i++){
+//     console.log(n)
+//     for (x=0;x<6;x++){
+//         if (x == 5){
+//             n+='*'
+//         }
+//        }    
+//     }
+
+// for (i=1;i<7;i++){
+//     let j=''
+//     for (y=1; y<=i; y++){
+//         j+='*'
+//     }
+//     console.log(j);
+// }
