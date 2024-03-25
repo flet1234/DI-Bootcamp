dc30967471d0db382ad20ad82276b1b9637e18c9
flet1234 <flet1908@gmail.com>
2024-03-25 15:47:28 +0200

    w5d2 Daily_challenge_Gold

---
+++ Week5/Day1/Daily_challenge_Gold/script.js
@@ -0,0 +1,19 @@
+// const numbers = [5,0,9,1,7,4,2,6,3,8];
+// let x = numbers.toString()
+// console.log(x);
+// let y = numbers.join(' _|_ ')
+// console.log(y);
+// let x
+// let z
+// for (i=0;i<numbers.length;i++){
+//     for (y=i+1; y < numbers.length;y++){
+//         if (numbers[i] < numbers [y]){
+//             x=numbers[i]
+//             z=numbers[y]
+//             numbers[i]=z
+//             numbers[y]=x
+//         }
+//     }
+// }
+   
+// console.log(numbers);
