f0d74935dd403ba28fdd23af0255c923ee98db2a
flet1234 <flet1908@gmail.com>
2024-03-25 13:41:23 +0200

    Daily_challenge Not bad

---
+++ Week5/Day1/Daily_challenge not bad/script.js
@@ -0,0 +1,8 @@
+// let sentence =  'This dinner is not that bad ! You cook well'
+// let wordNot = sentence.indexOf('not')
+// let wordBad = sentence.indexOf('bad')
+// if (wordNot != -1 && wordBad>wordNot){
+//     sentence = sentence.substring(0,wordNot)+'good'+(sentence.substring(wordBad+3))
+// }
+// console.log(sentence);
+
