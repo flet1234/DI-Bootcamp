1ac0808d3201cdf862ea4f0ad5632564be62c98a
flet1234 <flet1908@gmail.com>
2024-03-25 16:07:00 +0200

    w5d2 ExerciseGold

---
+++ Week5/Day1/ExerciseGold/script.js
@@ -0,0 +1,38 @@
+// #1
+// let numbers = [123, 8409, 100053, 333333333, 7]
+// for (x in numbers){
+//     if (x%3==0){
+//         console.log(true);
+//     } else {console.log(false);}
+// }
+
+// #2
+// let guestList = {
+//     randy: "Germany",
+//     karla: "France",
+//     wendy: "Japan",
+//     norman: "England",
+//     sam: "Argentina"
+//   }
+
+// let x = prompt('What is your name?: ')
+// if (x in guestList){
+//     console.log("Hi! I'm "+x+", and I'm from "+guestList[x]);
+// } else {
+//     console.log("Hi! I'm a guest.");
+// }
+
+// #3
+// let age = [20,5,12,43,98,55];
+// let x=0
+// for (i=0;i<age.length;i++){
+//     x+=age[i]
+// }
+// console.log(x);
+
+// for (i of age){
+//     if (i>x){
+//         x=i
+//     }
+// }
+//  console.log(x);
