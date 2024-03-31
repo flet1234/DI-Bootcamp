c457fb07db38ae21521c38ea4ad92277c53c7af9
flet1234 <flet1908@gmail.com>
2024-03-31 16:08:06 +0300

    w6d1

---
+++ Week6/Day1/Ninja/script.js
@@ -0,0 +1,36 @@
+// Exercise 1 : Merge Words
+// Instructions
+// Create a function such as mergeWords('Hello')() that returns the following string: 'Hello'
+// When the function is called without any arguments return a string where all words have been merged into a sentence.
+
+// For example
+// mergeWords('There')('is')('no')('spoon.')();
+// should return 'There is no spoon.'
+// Below is a verbose JavaScript solution, turn this into a currying function.
+
+// function mergeWords(string) {
+//  return function(nextString) {
+//    if (nextString === undefined) {
+//      return string;
+//    } else {
+//      return mergeWords(string + ' ' + nextString);
+//    }
+//  }
+// }
+// -->
+
+// function mergeWords(string) {
+//     return function(nextString) {
+//       if (nextString === undefined) {
+//         return string;
+//       } else {
+//         return mergeWords(string + ' ' + nextString);
+//       }
+//     }
+//    }
+
+// console.log(mergeWords('There')('is')('no')('spoon.')());
+
+let mergeWords = (string) => (newstring) => newstring === undefined ? string:mergeWords(string+' '+newstring)
+
+console.log(mergeWords('There')('is')('no')('spoon.')())
