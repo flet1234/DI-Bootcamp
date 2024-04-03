5742f7eea029cfd1cb63cc015ca88c346ac58ea2
flet1234 <flet1908@gmail.com>
2024-04-03 15:32:37 +0300

    copyed task

--- Week6/Day4/Daily_challenge_True/script.js
+++ Week6/Day4/Daily_challenge_True/script.js
@@ -1,3 +1,13 @@
+// Create a function that returns true if all parameters are truthy, and false otherwise.
+
+// Examples
+// allTruthy(true, true, true) ➞ true
+
+// allTruthy(true, false, true) ➞ false
+
+// allTruthy(5, 4, 3, 2, 1, 0) ➞ false
+
+
 function allTruthy (array){
     count=0
     array.forEach(item => {
---
+++ Week6/Day4/ExerciseGold/script.js
@@ -0,0 +1,11 @@
+// Exercise 1 : HTML Form #3
+// Instructions
+// form
+
+// Create a form like the form provided above. The form should contain three inputs:
+// name,
+// lastname,
+// submit
+// Send the data to another HTML file and display the sent data to a section tag, using the DOM.
+// -->
+
