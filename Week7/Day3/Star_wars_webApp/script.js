9d0359abd992d944403d9b359d60b1697d9a2fc1
flet1234 <flet1908@gmail.com>
2024-04-09 10:39:48 +0300

    w7d3 MP_starWars_webApp

---
+++ Week7/Day3/Star_wars_webApp/script.js
@@ -0,0 +1,36 @@
+async function getInfo(e) {
+    let load=document.getElementById('load')
+    let root=document.getElementById('info')
+    root.style.display='none'
+    load.style.display='block'
+    let url='https://www.swapi.tech/api/people'
+    let random=Math.floor(Math.random()*84)
+    let newUrl=`${url}/${random}`
+    let res= await fetch(newUrl)
+    if (res.status!==200){
+        throw new Error(`Status :${res.status}`)
+    }
+    else{
+        let data=await res.json()
+        let result=data.result.properties
+        let name=result.name
+        let height=result.height
+        let gender=result.gender
+        let bithYear=result.birth_year
+        let planetUrl=result.homeworld
+        let planetRes= await fetch(planetUrl)
+        let planet
+        if (planetRes.status!==200){
+            throw new Error(`Status :${planetRes.status}`)
+        }
+        else{
+            let planetData=await planetRes.json()
+            planet=planetData.result.properties.name
+        }
+        load.style.display='none'
+        root.style.display='block'
+        root.innerHTML=`<h2>${name}</h2>\n<p>Height: ${height}</p>\n
+        <p>Gender: ${gender}</p>\n<p>Year of birth: ${bithYear}</p>\n
+        <p>Homeworld :${planet}</p>`
+    }
+}
