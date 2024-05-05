f1c294631c4f6a9c8d00d19a8ac547c96a3e0960
flet1234 <flet1908@gmail.com>
2024-05-05 15:12:48 +0300

    w9d1 Daily_challnege

---
+++ Week9/Day1/Daily_challenge/daily_challenge/src/carousel.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+class DemoCarousel extends Component {
+    render() {
+        return (
+            <Carousel>
+                <div>
+                    <img style={{width:'250px'}} src="./assets/honk.jpg" />
+                    <p className="legend">Hong Kong</p>
+                </div>
+                <div>
+                    <img style={{width:'250px'}} src="./assets/kakao.webp" />
+                    <p className="legend">Makao</p>
+                </div>
+                <div>
+                    <img style={{width:'250px'}} src=".\assets\newyolk.webp" />
+                    <p className="legend">New York</p>
+                </div>
+                <div>
+                    <img style={{width:'250px'}} src=".\assets\tower.webp" />
+                    <p className="legend">Japan</p>
+                </div>
+            </Carousel>
+        );
+    }
+};
+
+export default DemoCarousel
